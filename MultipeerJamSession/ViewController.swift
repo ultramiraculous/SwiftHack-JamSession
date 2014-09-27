06d68af2826495d87d9dfd583820084bb7f9a515
Chris Williams <williamsc@fb.com>
2014-09-27 12:30:19 -0700

    Added initial project

---
+++ MultipeerJamSession/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MultipeerJamSession
+//
+//  Created by Chris Williams on 9/27/14.
+//  Copyright (c) 2014 SwiftHack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
