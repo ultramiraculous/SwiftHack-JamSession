45ae9d05910f2219cc9ff0abceb553867d948f88
Chris Williams <williamsc@fb.com>
2014-09-27 12:54:58 -0700

    Added connection manger

---
+++ MultipeerJamSession/ConnectionManger.swift
@@ -0,0 +1,9 @@
+//
+//  ConnectionManger.swift
+//  MultipeerJamSession
+//
+//  Created by Chris Williams on 9/27/14.
+//  Copyright (c) 2014 SwiftHack. All rights reserved.
+//
+
+import Foundation
