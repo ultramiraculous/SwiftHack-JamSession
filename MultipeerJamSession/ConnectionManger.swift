17bcae7d1a42bb6134d7a03fc97f048628b0f947
Chris Williams <williamsc@fb.com>
2014-09-27 13:07:38 -0700

    Added server and basic peer

--- MultipeerJamSession/ConnectionManger.swift
+++ MultipeerJamSession/ConnectionManger.swift
@@ -1,9 +1,30 @@
-//
 //  ConnectionManger.swift
 //  MultipeerJamSession
-//
-//  Created by Chris Williams on 9/27/14.
-//  Copyright (c) 2014 SwiftHack. All rights reserved.
-//
+
 
 import Foundation
+import MultipeerConnectivity
+
+//The peer name for our local device
+let JamSessionPeer = MCPeerID(displayName: UIDevice.currentDevice().name)
+
+
+class JamSessionServer : NSObject, MCNearbyServiceAdvertiserDelegate {
+    let serverName: String
+    let advertiser: MCNearbyServiceAdvertiser
+    
+    init(serverName: String){
+        self.serverName = serverName
+        self.advertiser = MCNearbyServiceAdvertiser(peer: JamSessionPeer,
+            discoveryInfo: nil,
+            serviceType: serverName)
+    }
+    
+    func advertiser(advertiser: MCNearbyServiceAdvertiser!, didReceiveInvitationFromPeer peerID: MCPeerID!, withContext context: NSData!, invitationHandler: ((Bool, MCSession!) -> Void)!) {
+        
+        
+        
+    }
+    
+    
+}
